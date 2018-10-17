15987c6dcefd544e1a4e7aac62d7bf93c638dbb5
takabatakeaoto <tkbtkaot@gmail.com>
2018-10-17 14:00:37 +0900

    no message

---
+++ Assets/Script/Patrol.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.AI;
+
+public class Patrol : MonoBehaviour
+{
+
+    public Transform[] points;
+    private int destPoint = 0;
+    private NavMeshAgent agent;
+
+    void Start()
+    {
+
+        agent = GetComponent<NavMeshAgent>();
+        agent.autoBraking = false;
+        GoToNextPoint();
+    }
+
+    void GoToNextPoint()
+    {
+        if (points.Length == 0)
+            return;
+
+        agent.destination = points[destPoint].position;
+        destPoint = (destPoint + 1) % points.Length;
+    }
+
+    void Update()
+    {
+        if (agent.remainingDistance < 0.5f)
+            GoToNextPoint();
+    }
+}
