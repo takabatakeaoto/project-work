d68a391165fa313376d161cf2e8bafd66c28eda1
takabatakeaoto <tkbtkaot@gmail.com>
2018-10-19 10:54:50 +0900

    Firework

---
+++ Assets/fireworks.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class fireworks : MonoBehaviour {
+    public GameObject Fire;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if(ScoreManager.score < 10)
+        {
+            Fire.SetActive(false);
+        }
+
+
+        if (ScoreManager.score > 10)
+        {
+            Fire.SetActive(true);
+        }
+	}
+}
